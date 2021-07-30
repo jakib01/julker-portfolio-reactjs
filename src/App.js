18b1e54c7ad33ab84b9fcfdb13b8d782de2301a5
jakib01 <julkernienakib@gmail.com>
2021-07-30 18:21:27 +0600

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import './App.css';
-import './css/bootstrap.min.css';
-import './css/all.min.css';
+
+// import './css/bootstrap.min.css';
+// import './css/all.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import './css/resume.min.css';
-import jQuery from 'jquery';
-import './js/jquery.min.js';
-import './js/bootstrap.bundle.min.js';
-import './js/jquery.easing.min.js';
-// import './js/resume.min.js';
-import Home from './components/Home'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <Home />
+    <Navbar />
   );
 }
 
