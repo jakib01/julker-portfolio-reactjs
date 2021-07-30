6685584c4bb7c22deb171b56ccd8604999e94146
jakib01 <julkernienakib@gmail.com>
2021-07-31 00:48:09 +0600

    fontawesome

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-
 // import './css/bootstrap.min.css';
 // import './css/all.min.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
 import './css/resume.min.css';
 import Navbar from './components/Navbar'
 import Home from './components/Home';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
 function App() {
-  return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar/>
-          <Switch>
-            <Route exact path="/" component={Home}/>
-          </Switch>
-        </div>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Navbar/>
+                <div className="container-fluid p-0">
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                    </Switch>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,44 @@
 import React from "react";
 
-
 function Home(props) {
     return (
-        <div className="container-fluid p-0">
-        <h1>home</h1>
-        </div>
+        <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
+            <div className="w-100">
+                <h1 className="mb-0">Julker Nien
+                    <span className="text-primary">Akib</span>
+                </h1>
+                <div className="subheading mb-5"> Dhaka, Bangladesh · +8801673952895 ·
+                    <a href="mailto:name@email.com">julkernienakib@gmail.com</a>
+                </div>
+                <p className="lead mb-5">Extremely enthusiastic about Robotics and related technologies. My areas of
+                    expertise
+                    are Robotics, IoT, AI and machine learning. About 9+ robotics projects completed in my grad life
+                    with
+                    great achievements. Passionate about innovations and technological changes. I am looking for an
+                    opportunity in Robotics, IoT, AI and machine learning sector where I can use my skills in developing
+                    strategies and leading a team.</p>
+                <div className="social-icons">
+                    <a href="https://www.facebook.com/akib.jn">
+                        <i className="fab fa-facebook-f"></i>
+                    </a>
+                    <a href="https://www.linkedin.com/in/julker-nien-akib-3110a5132/">
+                        <i className="fab fa-linkedin-in"></i>
+                    </a>
+                    <a href="https://github.com/jakib01">
+                        <i className="fab fa-github"></i>
+                    </a>
+                    <a href="https://www.researchgate.net/profile/Julker_Akib">
+                        <i className="fab fa-researchgate"></i>
+                    </a>
+                    <a href="https://www.facebook.com/teamrobux/">
+                        <i className="fab fa-facebook-f"></i>
+                    </a>
+
+                </div>
+            </div>
+        </section>
+
+    // <hr className="m-0">
     );
 }
 
