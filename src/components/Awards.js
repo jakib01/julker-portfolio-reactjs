3dc961947cb36830d456934dcb3e56d9e1e3a5b9
jakib01 <julkernienakib@gmail.com>
2021-08-03 13:05:06 +0600

    award

--- src/components/Awards.js
+++ src/components/Awards.js
@@ -3,7 +3,48 @@ import React from "react";
 
 function Awards(props) {
     return (
-        <h1>awards</h1>
+        <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="awards">
+            <div className="w-100">
+                <h2 className="mb-5">Awards &amp; Certifications</h2>
+                <ul className="fa-ul mb-0">
+                    <li>
+                        <i className="fa-li fa fa-trophy text-warning"></i>
+                        2017 Achieve 1st prize in Robowars at Techfest Bangladesh Round at National Robotech Festival.
+                        Dhaka, Bangladesh.
+                    </li>
+                    <li>
+                        <i className="fa-li fa fa-trophy text-warning"></i>
+                        2017 Achieve 1st prize in Robo Soccer at EEE Day at UIU EEE Dept. Dhaka, Bangladesh.
+                    </li>
+                    <li>
+                        <i className="fa-li fa fa-trophy text-warning"></i>
+                        2017 Achieve Special position for Robotic Arms at UIU CSE Dept. present Electronics Project
+                        Show.
+                        Dhaka, Bangladesh.
+                    </li>
+                    <li>
+                        <i className="fa-li fa fa-trophy text-warning"></i>
+                        2017 Participated International competition Techfest presented Robowars. Mumbai, India.
+                    </li>
+                    <li>
+                        <i className="fa-li fa fa-trophy text-warning"></i>
+                        2018 Achieve 1st prize in Robo Soccer at IEEE Day at UIU EEE Dept. Dhaka, Bangladesh.
+                    </li>
+                    <li>
+                        <i className="fa-li fa fa-trophy text-warning"></i>
+                        2018 Achieve 1st prize in CISCO Networking Academy presented the Internet of things (IoT)
+                        Project
+                        Exhibition at United International University. Dhaka, Bangladesh.
+                    </li>
+                    <li>
+                        <i className="fa-li fa fa-trophy text-warning"></i>
+                        2018 Organize Techfest Bangladesh Round at United International University held on November
+                        2018,
+                        Dhaka, Bangladesh.
+                    </li>
+                </ul>
+            </div>
+        </section>
     );
 }
 
