1eec97c82226ca8d09f0919b6f7ffe05ef9d6050
jakib01 <julkernienakib@gmail.com>
2021-08-03 12:34:45 +0600

    skills

--- src/components/Education.js
+++ src/components/Education.js
@@ -3,7 +3,46 @@ import React from "react";
 
 function Education(props) {
     return (
-        <h1>education</h1>
+        <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="education">
+            <div className="w-100">
+                <h2 className="mb-5">Education</h2>
+
+                <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div className="resume-content">
+                        <h3 className="mb-0">United International university.</h3>
+                        <div className="subheading mb-3">Bachelor of Science</div>
+                        <div>Computer Science - Robotics, IoT and Mechine Learning Track</div>
+                        {/*<p>GPA: 3.23</p>*/}
+                    </div>
+                    <div className="resume-date text-md-right">
+                        <span className="text-primary">August 2014 - December 2019</span>
+                    </div>
+                </div>
+
+                <div className="resume-item d-flex flex-column flex-md-row justify-content-between">
+                    <div className="resume-content">
+                        <h3 className="mb-0">Saint Joseph Higher Secondary School</h3>
+                        <div className="subheading mb-3">HSC. in Science</div>
+                        {/*<p>GPA: 3.56</p>*/}
+                    </div>
+                    <div className="resume-date text-md-right">
+                        <span className="text-primary">August 2011 -  August 2013</span>
+                    </div>
+                </div>
+
+                <div className="resume-item d-flex flex-column flex-md-row justify-content-between">
+                    <div className="resume-content">
+                        <h3 className="mb-0">Mohammadpur Model School And College</h3>
+                        <div className="subheading mb-3">SSC. in Science</div>
+                        {/*<p>GPA: 3.56</p>*/}
+                    </div>
+                    <div className="resume-date text-md-right">
+                        <span className="text-primary">January 2000 -  December 2011</span>
+                    </div>
+                </div>
+
+            </div>
+        </section>
     );
 }
 
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,157 @@
-
 import React from "react";
 
 function Skills(props) {
     return (
-        <h1>Skills</h1>
+        <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="skills">
+            <div className="w-100">
+
+                <div className="subheading mb-3">Programming Languages &amp; Tools</div>
+                <ul className="list-inline dev-icons">
+                    <li className="list-inline-item">
+                        <i className="fab fa-html5"></i>
+                    </li>
+                    <li className="list-inline-item">
+                        <i className="fab fa-css3-alt"></i>
+                    </li>
+
+                    <li className="list-inline-item">
+                        <i className="fab fa-android"></i>
+                    </li>
+                    <li className="list-inline-item">
+                        <i className="fab fa-php"></i>
+                    </li>
+                    <li className="list-inline-item">
+                        <i className="fab fa-python"></i>
+                    </li>
+                    <li className="list-inline-item">
+                        <i className="fab fa-wordpress"></i>
+                    </li>
+                    <li className="list-inline-item">
+                        <i className="fab fa-react"></i>
+                    </li>
+                    <li className="list-inline-item">
+                        <i className="fab fa-node-js"></i>
+                    </li>
+                    <li className="list-inline-item">
+                        <i className="fab fa-npm"></i>
+                    </li>
+                </ul>
+
+                <div className="subheading mb-3">Languages</div>
+                <ul className="fa-ul mb-0">
+                    <li>
+                        <i className="fa-li fa fa-check"></i>
+                        PHP
+                    </li>
+                    <li>
+                        <i className="fa-li fa fa-check"></i>
+                        Javascript
+                    </li>
+                    <li>
+                        <i className="fa-li fa fa-check"></i>
+                        C/C++
+                    </li>
+                    <li>
+                        <i className="fa-li fa fa-check"></i>
+                        Hardware Programming
+                    </li>
+                    <li>
+                        <i className="fa-li fa fa-check"></i>
+                        HTML/CSS(markup language)
+                    </li>
+
+                    <li>
+                        <i className="fa-li fa fa-check"></i>
+                        Mobile Application (Android)
+                    </li>
+                    <li>
+                        <i className="fa-li fa fa-check"></i>
+                        Python
+                    </li>
+                    <li>
+                        <i className="fa-li fa fa-check"></i>
+                        Java
+                    </li>
+                </ul>
+
+
+                <div className="subheading mb-3">Skills</div>
+
+                <div className="row">
+                    <div className="col-md-6">
+                        <ul className="fa-ul mb-0">
+                            <li>
+                                <i className="fa-li fa fa-check"></i>
+                                Laravel
+                            </li>
+                            <li>
+                                <i className="fa-li fa fa-check"></i>
+                                React js
+                            </li>
+                            <li>
+                                <i className="fa-li fa fa-check"></i>
+                                Jquery
+                            </li>
+                            <li>
+                                <i className="fa-li fa fa-check"></i>
+                                MySQL
+                            </li>
+                            <li>
+                                <i className="fa-li fa fa-check"></i>
+                                ORM
+                            </li>
+                            <li>
+                                <i className="fa-li fa fa-check"></i>
+                                HTML/CSS
+                            </li>
+                            <li>
+                                <i className="fa-li fa fa-check"></i>
+                                Unit Test
+                            </li>
+                            <li>
+                                <i className="fa-li fa fa-check"></i>
+                                Mobile Application (Android)
+                            </li>
+                        </ul>
+                    </div>
+
+
+                    <div className="col-md-6">
+                        <ul className="fa-ul mb-0">
+                            <li>
+                                <i className="fa-li fa fa-check"></i>
+                                Restful API,
+                            </li>
+                            <li>
+                                <i className="fa-li fa fa-check"></i>
+                                Vue js
+                            </li>
+                            <li>
+                                <i className="fa-li fa fa-check"></i>
+                                Ajax
+                            </li>
+                            <li>
+                                <i className="fa-li fa fa-check"></i>
+                                Firebase
+                            </li>
+                            <li>
+                                <i className="fa-li fa fa-check"></i>
+                                Bootstrap
+                            </li>
+                            <li>
+                                <i className="fa-li fa fa-check"></i>
+                                Hardware Programming
+                            </li>
+                            <li>
+                                <i className="fa-li fa fa-check"></i>
+                                Flask
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </section>
     );
 }
 
