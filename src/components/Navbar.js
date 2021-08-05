02a3d36f3abff4796159f7ee2b05c3acbb42360e
jakib01 <julkernienakib@gmail.com>
2021-08-05 18:35:18 +0600

    navbar done working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,8 +25,8 @@ function Navbar(props) {
     // </Navbar>
 
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
-            <a className="navbar-brand js-scroll-trigger" href="#page-top">
-                <span className="d-block d-lg-none">Julker Nien Akib</span>
+            <a className="navbar-brand js-scroll-trigger" href="/">
+                <span className="d-block d-lg-none ml-2"> Julker Nien Akib </span>
                 <span className="d-none d-lg-block">
                     <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="images/profile.jpg" alt=""/>
                 </span>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
 import './css/resume.min.css';
-import "../node_modules/jquery/dist/jquery.min.js";
-import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+// import "../node_modules/jquery/dist/jquery.min.js";
+// import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
